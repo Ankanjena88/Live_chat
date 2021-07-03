aedca8b403fbe17628967ef81484ba6628672f18
Ankanjena88 <66504046+Ankanjena88@users.noreply.github.com>
2021-07-03 17:53:01 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from "react";
+import Login from "./components/login";
+// import Welcome from './components/welcome'
+const App = () => {
+  return (
+    <>
+      <Login />
+      {/* <Welcome /> */}
+    </>
+  );
+};
+
+export default App;
